97e82f14045de39a4f0b26a0764f2c83be9b138d
Priya James <priya.james@Priyas-MBP.bbrouter>
2022-12-22 16:17:27 +0530

    Modify. Signing and Verification

--- src/api/Api.js
+++ src/api/Api.js
@@ -17,12 +17,16 @@ const doPost = (url, body) => {
   );
 
   const signature = crypto.sign(null, Buffer.from(signingString), privateKey);
+  console.log("The signature is " + signature);
+
+  const header = createAuthorizationHeader(signature.toString("base64"));
+  console.log(typeof(header));
 
   return fetch(url, {
     method: "post",
     body: JSON.stringify(body),
     headers: {
-      "X-Gateway-Authorization": signature.toString("base64"),
+      "X-Gateway-Authorization": header,
       "Content-Type": "application/json",
     },
   });
@@ -31,6 +35,19 @@ const doPost = (url, body) => {
 const createSigningString=(body)=>{
   return JSON.stringify(body);
 }
+
+//-------------------------
+
+const createAuthorizationHeader = (signature) => {
+  const created = Math.floor(new Date().getTime() / 1000 - 1 * 60).toString(); //TO USE IN CASE OF TIME ISSUE
+  const expires = (parseInt(created) + 1 * 60 * 60).toString(); //Add required time to create expired
+
+  const header = `Signature keyId="1001|UUID|ed25519",algorithm="ed25519",created="${created}",expires="${expires}",headers="(created) (expires) digest",signature="${signature}"`;
+  console.log(header);
+  return header;
+};
+
+//--------------
 
 export default {
   doPost,
