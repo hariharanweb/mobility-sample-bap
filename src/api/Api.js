a270ee2f008a174a767c6b22e407552b777aae5e
shashank.sharma <Shashank_A_Sharma@gap.com>
2022-12-27 14:41:57 +0530

    Added changes for libsodium and full signature
    libsodium is the library which will be being used
    and header section for X-Gateway-Authorization has been updated with complete string with full signature

--- src/api/Api.js
+++ src/api/Api.js
@@ -1,46 +1,63 @@
-import fetch from "node-fetch";
-import LoggingService from "../services/LoggingService";
-import crypto from "crypto";
-import fs from "fs";
-import path from "path";
+import fetch from 'node-fetch';
+// eslint-disable-next-line camelcase
+import sodium from 'libsodium-wrappers';
+import crypto from 'crypto';
+import fs from 'fs';
+import path from 'path';
+import LoggingService from '../services/LoggingService';
 
-const doPost = (url, body) => {
-  const logger = LoggingService.getLogger("API");
+const doPost = async (url, body) => {
+  const logger = LoggingService.getLogger('API');
   logger.debug(`Posting to ${url} with Content ${JSON.stringify(body)}`);
 
-  const identity = {};
-  const signingString = createSigningString(body);
-
   const privateKey = fs.readFileSync(
-    path.resolve("./src/api/CertificateBAP/privateKey.txt"),
-    "utf-8"
+    path.resolve('./src/api/CertificateBAP/privateKey.txt'),
+    'utf-8',
   );
 
-  const signature = crypto.sign(null, Buffer.from(signingString), privateKey);
-  console.log("The signature is " + signature);
+  const signature = await createSignatureWithSodium(JSON.stringify(body));
+  console.log(`The signature is ${signature}`);
 
-  const header = createAuthorizationHeader(signature.toString("base64"));
-  console.log(typeof(header));
+  const gatewayAuthHeaderValue = createAuthorizationHeader(signature);
 
   return fetch(url, {
-    method: "post",
+    method: 'post',
     body: JSON.stringify(body),
     headers: {
-      "X-Gateway-Authorization": header,
-      "Content-Type": "application/json",
+      'X-Gateway-Authorization': gatewayAuthHeaderValue,
+      'Content-Type': 'application/json',
     },
   });
 };
 
-const createSigningString=(body)=>{
-  return JSON.stringify(body);
-}
+const createSignature = (body) => crypto.sign(null, Buffer.from(body), privateKey);
+
+const createSignatureWithSodium = async (body) => {
+  const created = Math.floor(
+    new Date().getTime() / 1000 - 1 * 60,
+  ).toString();
+  const expires = (parseInt(created) + 1 * 60 * 60).toString();
+
+  await sodium.ready;
+  const digest = sodium.crypto_generichash(64, sodium.from_string(body));
+  const digest_base64 = sodium.to_base64(digest, sodium.base64_variants.ORIGINAL);
+  const signing_string = `(created): ${created}
+(expires): ${expires}
+digest: BLAKE-512=${digest_base64}`;
+  const privateKey = `${process.env.privateKey}`;
+  console.log(`private key is :${privateKey}`);
+  const signedMessage = sodium.crypto_sign_detached(
+    signing_string,
+    sodium.from_base64(privateKey, sodium.base64_variants.ORIGINAL),
+  );
+  return sodium.to_base64(signedMessage, sodium.base64_variants.ORIGINAL);
+};
 
 //-------------------------
 
 const createAuthorizationHeader = (signature) => {
-  const created = Math.floor(new Date().getTime() / 1000 - 1 * 60).toString(); //TO USE IN CASE OF TIME ISSUE
-  const expires = (parseInt(created) + 1 * 60 * 60).toString(); //Add required time to create expired
+  const created = Math.floor(new Date().getTime() / 1000 - 1 * 60).toString(); // TO USE IN CASE OF TIME ISSUE
+  const expires = (parseInt(created) + 1 * 60 * 60).toString(); // Add required time to create expired
 
   const header = `Signature keyId="1001|UUID|ed25519",algorithm="ed25519",created="${created}",expires="${expires}",headers="(created) (expires) digest",signature="${signature}"`;
   console.log(header);
