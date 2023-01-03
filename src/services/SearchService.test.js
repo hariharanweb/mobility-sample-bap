252faa827ff43ec835300cb83747d1a3f8253855
shashank.sharma <Shashank_A_Sharma@gap.com>
2023-01-03 17:38:45 +0530

    remove unwanted test

--- src/services/SearchService.test.js
+++
@@ -1,8 +0,0 @@
-const SearchService = require("./SearchService");
-
-describe("Sample testing", () => {
-    it("should check for search funtion", () => {
-        // const type = await (typeof SearchService.search("hello"));
-        // expect(type).toBe("function");
-    });
-});
