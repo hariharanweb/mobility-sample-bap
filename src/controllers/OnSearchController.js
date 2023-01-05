19a3964612ecf546a5ccef4a7e0b3ae2acb483e1
priyajames29 <42799848+priyajames29@users.noreply.github.com>
2023-01-05 16:34:08 +0530

    Add. Cache publicKey in LookUpService

--- src/controllers/OnSearchController.js
+++ src/controllers/OnSearchController.js
@@ -1,23 +1,27 @@
-import LoggingService from '../services/LoggingService';
-import SearchService from '../services/SearchService';
-import genericResponse from '../utilities/GenericResponse';
-import authVerifier from '../utilities/SignVerify/AuthHeaderVerifier';
-import LookUpService from '../services/LookUpService';
+import LoggingService from "../services/LoggingService";
+import SearchService from "../services/SearchService";
+import genericResponse from "../utilities/GenericResponse";
+import authVerifier from "../utilities/SignVerify/AuthHeaderVerifier";
+import LookUpService from "../services/LookUpService";
 
-const BecknGateway = 'BG';
+const Subcriber_ID = "Gateway";
+const ukID = "111-222-303";
 const onSearch = async (req, res) => {
-  const logger = LoggingService.getLogger('OnSearchController');
+  const logger = LoggingService.getLogger("OnSearchController");
   logger.debug(`on_search called with ${JSON.stringify(req.body)}`);
 
-  const publicKey = await LookUpService.getPublicKey(BecknGateway); 
-  authVerifier.authorize(req, publicKey).then(() => {
-    logger.debug('Request Authorized Successfully.');
-    SearchService.storeSearchResult(req.body);
-    genericResponse.sendAcknowledgement(res);
-  }).catch((err) => {
-    logger.error(`Authorization Failed ${err}`);
-    genericResponse.sendErrorWithAuthorization(res);
-  });
+  const publicKey = await LookUpService.getPublicKey(ukID);
+  authVerifier
+    .authorize(req, publicKey)
+    .then(() => {
+      logger.debug("Request Authorized Successfully.");
+      SearchService.storeSearchResult(req.body);
+      genericResponse.sendAcknowledgement(res);
+    })
+    .catch((err) => {
+      logger.error(`Authorization Failed ${err}`);
+      genericResponse.sendErrorWithAuthorization(res);
+    });
 };
 
 export default {
--- src/services/LookUpService.js
+++ src/services/LookUpService.js
@@ -1,14 +1,31 @@
 import Api from "../api/Api";
+import Cache from "../utilities/Cache";
 
 const REGISTRY_URL = `${process.env.REGISTRY_URL}/lookup`;
 
-const getPublicKey = async (type) => {
-  const request = JSON.stringify({
-    type: type,
-  });
 
+const getPublicKey = async (ukId) => {
+  const cachekey = `publicKey - ${ukId};`;
+  const publicKey = await Cache.getCache(cachekey);
+  if (publicKey) {
+    console.log("The cached publicKey is:" + publicKey);
+    return publicKey;
+  }
+  return lookUpPublicKey(cachekey, ukId);
+};
+
+const lookUpPublicKey = async (cachekey, ukId) => {
+  const request = JSON.stringify({
+    ukId: ukId,
+  });
+  console.log("The request is" + request);
   const response = await Api.doPost(REGISTRY_URL, request);
   const responseJson = await response.json();
+  Cache.setCache(cachekey, responseJson[0].signing_public_key);
+  console.log(
+    "the looked up public cachekey is: " + cachekey,
+    responseJson[0].signing_public_key
+  );
   return responseJson[0].signing_public_key;
 };
 
